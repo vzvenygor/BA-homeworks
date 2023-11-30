2c94c71688b48b8e670501a748e40160d2011211
Volodymyr Zvenyhorodskyi <vzvenygorv@gmail.com>
2023-11-30 16:00:07 +0200

    Fixed homework JS:base

--- homeworks/29_JS-base/index.js
+++ homeworks/29_JS-base/index.js
@@ -1,31 +1,31 @@
 //Норма//
 
-const USER_NAME = prompt("Як тебе звати?");
-console.log(`Привіт, ${USER_NAME}!`);
+const userName = prompt("Як тебе звати?");
+console.log(`Привіт, ${userName}!`);
 
-const YEAR = +prompt(`${USER_NAME}, введи свій рік народження`);
+const year = +prompt(`${year}, введи свій рік народження`);
 const CURRENT_YEAR = 2023;
-let age = CURRENT_YEAR - YEAR;
+let age = CURRENT_YEAR - year;
 console.log(`Гм, значить тобі ${age} років`);
 
-const LENGTH = +prompt(`${USER_NAME}, введи довжину сторони квадрату`);
-let perimeter = 4 * LENGTH;
+const length = +prompt(`${userName}, введи довжину сторони квадрату`);
+let perimeter = 4 * length;
 console.log(`Периметр такого квадрата рівний ${perimeter}`);
 
 //Максимум//
 
-const RADIUS = +prompt("Введи радіус кола");
-let circleArea = Math.PI * RADIUS ** 2;
+const radius = +prompt("Введи радіус кола");
+let circleArea = Math.PI * radius ** 2;
 console.log(`Площа такого кола становитиме ${circleArea}`);
 
-const LENGTH_CITY = +prompt("Веди відстань між двома містами у кілометрах");
+const lenghtCity = +prompt("Веди відстань між двома містами у кілометрах");
 const time = +prompt("За скільки годин ти хочеш дістатися до іншого міста?");
-let speed = LENGTH_CITY / time;
+let speed = lenghtCity / time;
 console.log(
   `Щоб встигнути вчасно, тобі потрібно рухатися зі швидкісттю ${speed} км/год`
 );
 
-const DOLLARS = +prompt("Скільки доларів бажаєш обміняти на євро?");
-const EXCHANGE_RATE = 0.92;
-let counter = DOLLARS * EXCHANGE_RATE;
-console.log(`За ${DOLLARS} доларів ти зможеш отримати ${counter} євро`);
+const dollars = +prompt("Скільки доларів бажаєш обміняти на євро?");
+const exchangeRate = 0.92;
+let counter = dollars * exchangeRate;
+console.log(`За ${dollars} доларів ти зможеш отримати ${counter} євро`);
