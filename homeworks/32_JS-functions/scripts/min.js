66f92d062e146ef3e3d3f1c31fd7896c72d8f309
Volodymyr Zvenyhorodskyi <vzvenygorv@gmail.com>
2023-12-02 11:46:49 +0200

    Fixed bugs in JS:functions

--- homeworks/32_JS-functions/scripts/min.js
+++ homeworks/32_JS-functions/scripts/min.js
@@ -2,27 +2,27 @@
 
 // //1. Напиши всі можливі варіанти створення функцій.
 
-// function functionNameDeclaration (arguments) {
-//     //function body
+// function functionNameDeclaration(arguments) {
+//   //function body
 // } //function declaration
 
 // const functionNameExpression = function (arguments) {
-//     //function body
+//   //function body
 // }; //function expression
 
 // (arguments) => {
-//     //function body
-// } //arrow function
+//   //function body
+// }; //arrow function
 
 // element.map((arguments) => {
-//     //function body
-// }) //short function
+//   //function body
+// }); //short function
 
 //2. Створи функцію, яка буде виводити кількість переданих їй аргументів.
 
-// function setArgumentsCount () {
-//   console.log(arguments.length);
-// }
+function setArgumentsCount() {
+  console.log(arguments.length);
+}
 
 // setArgumentsCount(2, 4, "hkgkj");
 
@@ -31,60 +31,71 @@
 1 - якщо перше число більше, ніж друге; 
 0 - якщо числа рівні.*/
 
-// function setNum(num1, num2) {
-//   const a = num1 === num2;
+function setNum(num1, num2) {
+  if (typeof num1 !== "number" || isNaN(num1)) {
+    console.log("Не коректне перше число");
+  }
+  if (typeof num2 !== "number" || isNaN(num2)) {
+    console.log("Не коректне друге число");
+  }
 
-//   switch (a) {
-//     case num1 < num2:
-//       return "-1";
-//       break;
-//     case num1 > num2:
-//       return "1";
-//       break;
-//     case num1 === num2:
-//       return "0";
-//       break;
-//     default:
-//       return "Введіть коректні значення";
-//   }
-// }
+  const a = num1 === num2;
+
+  switch (a) {
+    case num1 < num2:
+      return "-1";
+    case num1 > num2:
+      return "1";
+    case num1 === num2:
+      return "0";
+    default:
+      return "Введіть коректні значення";
+  }
+}
 
 // console.log(setNum(3, 2));
 
 //4. Напиши функцію, яка обчислює факторіал переданого їй числа.
 
-// const setFactorial = function(num) {
-//   let factorial = 1;
+const setFactorial = function (num) {
+  let factorial = 1;
 
-//   for(let i = 1; i <= num; i++) {
-//     factorial *= i;
-//   }
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
 
-//   return factorial;
-// };
+  return factorial;
+};
 
 // console.log(setFactorial(5));
 
 /*5. Напиши функцію, яка приймає три окремі цифри і перетворює їх в одне число. 
 Наприклад: цифри 1, 4, 9 перетворяться в число 149.*/
 
-// function transformNum() {
-//   const anyNum = prompt("Ведіть три цифри, розділені комою").split(",", 3);
-//   let combine = anyNum[0] + anyNum[1] + anyNum[2];
-//   return combine;
-// }
+function transformNum() {
+  const anyNum = prompt("Ведіть три цифри, розділені комою").split(",", 3);
+  let combine = anyNum[0] + anyNum[1] + anyNum[2];
+  return Number(combine);
+}
 
-// console.log(+transformNum());
+// console.log(transformNum());
 
 /*Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу. 
 Якщо в функцію передали 1 параметр, то вона обчислює площу квадрата.*/
 
-// function setArea(a, b) {
-//   if (!a || !b) {
-//     b = a;
-//     return a * a;
-//   }
-//   return a * b;
-// }
+function setArea(a, b) {
+  if (typeof a !== "number" || isNaN(a)) {
+    console.log("Не коректне перше число");
+  }
+  if (typeof b !== "number" || isNaN(b)) {
+    console.log("Не коректне друге число");
+  }
 
-// console.log(setArea(2, 3));
+  if (!a || !b) {
+    b = a;
+    return a * a;
+  }
+  return a * b;
+}
+
+// console.log(setArea('2', 3));
--- homeworks/32_JS-functions/scripts/norm.js
+++ homeworks/32_JS-functions/scripts/norm.js
@@ -2,41 +2,47 @@
 
 //1. Напиши функцію, яка перевіряє, чи є передане їй число “досконалим числом”
 
-// function setPerfectNumber(num) {
-//   let sum = 0;
+function setPerfectNumber(num) {
+  let sum = 0;
 
-//   for (let i = 1; i <= num - 1; i++) {
-//     if (num % i === 0) {
-//       sum += i;
-//     }
-//   }
+  for (let i = 1; i <= num - 1; i++) {
+    if (num % i === 0) {
+      sum += i;
+    }
+  }
 
-//   if (num === sum) {
-//     return `Число ${num} є досконалим`;
-//   } else {
-//     return `Число ${num} не є досконалим`;
-//   }
-// }
+  if (num === sum) {
+    return `Число ${num} є досконалим`;
+  } else {
+    return `Число ${num} не є досконалим`;
+  }
+}
 
 // console.log(setPerfectNumber(6));
 
 /*2. Напиши функцію, яка приймає мінімальне і максимальне значення для діапазону, 
 і виводить тільки ті числа з діапазону, які є досконалими.*/
 
-// function isNumPerfect (x, y) {
+function isNumPerfect (x, y) {
+  if (typeof x !== "number" || isNaN(x)) {
+    console.log("Не коректне перше число");
+  }
+  if (typeof y !== "number" || isNaN(y)) {
+    console.log("Не коректне друге число");
+  }
 
-//   for (let i = x; i < y; i++) {
-//       let sum = 0;
+  for (let i = x; i < y; i++) {
+      let sum = 0;
 
-//       for (let j = 1; j <= i / 2; j++) {
-//           if (i % j === 0) {
-//               sum += j;
-//           }
-//       }
-//       if (i === sum) {
-//           console.log(i)
-//       }
-//   }
-// }
+      for (let j = 1; j <= i / 2; j++) {
+          if (i % j === 0) {
+              sum += j;
+          }
+      }
+      if (i === sum) {
+          console.log(i)
+      }
+  }
+}
 
 // isNumPerfect(1, 1000);
